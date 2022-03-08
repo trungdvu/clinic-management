ffb3b7d032f540b784a041d1f2c359195251331d
Trung Vu <trungdvu.it@gmail.com>
2022-03-08 18:38:35 +0700

    Add prettier and antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import './App.css';
+import { Button } from 'antd';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline">Hello world!</h1>
+    <div>
+      <Button type="primary">This is button from antd</Button>
+      <h1 className="text-3xl font-bold text-red-500 underline">Hello world!</h1>
+    </div>
   );
 }
