78f18e1481a9d5721835184408e20f1510bccb69
Trung Vu <trungdvu.it@gmail.com>
2022-04-30 03:20:28 +0700

    API: Update method patch to put

--- server/src/dtos/medical-bill/update.dto.ts
+++ server/src/dtos/medical-bill/update.dto.ts
@@ -1,5 +1,8 @@
+import { MedicalBillStatus } from "../../models";
+
 export interface UpdateMedicalBillDto {
   diseaseTypeIds?: string[];
   symptomDescription?: string;
   prediction?: string;
+  status?: MedicalBillStatus;
 }
